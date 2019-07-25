4fa4b72f9e378fa643aa65608f2c0d0175754a2d
ahalarewicz <alhalarewicz@gmail.com>
2019-07-24 22:56:30 -0700

    3 var system working successfuly

--- kirankelly/02lecture/systems3.java
+++ kirankelly/02lecture/systems3.java
@@ -1,4 +1,4 @@
-public class systems2{
+public class systems3{
 	public static void main(String[] args) {
 		//sample
 		// x = 3
@@ -23,55 +23,77 @@ public class systems2{
 		z2 =  9;
 		s2 =  2;
 
+		x3 =  1;
+		y3 =  4;
+		z3 =  3;
+		s3 =-10;
+
 		int eq1[] = {x1 , y1 , z1 , s1};
 		int eq2[] = {x2 , y2 , z2 , s2};
 		int eq3[] = {x3 , y3 , z3 , s3};
-		int eqy[] = {0  , 0  , 0  , 0 };
-		int eqz[] = {0  , 0  , 0  , 0 };
 		int num1;
 		int num2;
 		int num3;
 		
 		printeq(eq1 , eq2 , eq3);
 
-		// eliminate x-coefs to create a 2 var system
+		// create equal x coefficients
 		for (int i = 0; i < eq1.length; i++){
 			num1 = eq1[i];
 			num2 = eq2[i];
 			num3 = eq3[i];
-
-
+			num1 = num1 * x2 * x3;
+			num2 = num2 * x1 * x3;
+			num3 = num3 * x1 * x2;
+			eq1[i] = num1;
+			eq2[i] = num2;
+			eq3[i] = num3;
 		}
+		
+		printeq(eq1 , eq2 , eq3);
 
-		// mult by const to create matching  x coef
+		// create a 2-var system with y and z
 		for (int i = 0; i < eq1.length; i++){
 			num1 = eq1[i];
 			num2 = eq2[i];
-			num1 = num1 * x1 * x2;
-			num2 = num2 * x1 * x1;
-			eq1[i] = num1;
+			num3 = eq3[i];
+			eq2[i] = num1 - num2;
+			eq3[i] = num1 - num3;
+		}
+
+		printeq(eq1 , eq2 , eq3);
+
+		// mult by const to create matching y-coef
+		int a = eq2[1];
+		int b = eq3[1];
+		for (int i = 1; i < (eq2.length); i++){
+			num2 = eq2[i];
+			num3 = eq3[i];
+			num2 = num2 * b;
+			num3 = num3 * a;
 			eq2[i] = num2;
+			eq3[i] = num3;
 		}
 
-		printeq(eq1 , eq2);
+		printeq(eq1 , eq2 , eq3);
 
-		// subtract equations to eliminate x
-		for (int i=0 ; i < eq1.length; i++){
-			eqy[i] = eq1[i] - eq2[i];
+		// subtract equations to eliminate y
+		for (int i=0 ; i < eq2.length; i++){
+			eq3[i] = eq2[i] - eq3[i];
 		}
+		printeq(eq1 , eq2 , eq3);
+
+		// calc Z
+		Z = eq3[3]/eq3[2];
 
 		// calc Y
-		Y = eqy[2]/eqy[1];
+		Y = ( eq2[3] - (Z*eq2[2]) )/ eq2[1];
 
 		// calc X
-		s1 = s1 - ( Y * y1);
-		X = s1/x1;
+		X = ( eq1[3] - (Z*eq1[2]) - (Y*eq1[1]) )/ eq1[0];
 
 		// print X, Y, and Z
-		System.out.println("** x = "+X+"  &  y = "+Y+" **");
-
-
-
+		System.out.println("** x = "+X+"  &  y = "+Y+"  &  z = "+Z+" **");
 	}
 
 	public static void printeq(int eq1[], int eq2[], int eq3[]){
@@ -87,9 +109,9 @@ public class systems2{
 		int s2 = eq2[3];
 
 		int x3 = eq3[0];
-		int y2 = eq3[1];
-		int z2 = eq3[2];
-		int s1 = eq3[3];
+		int y3 = eq3[1];
+		int z3 = eq3[2];
+		int s3 = eq3[3];
 
 		System.out.println("_____________");
 		System.out.println(x1+"x + "+y1+"y + "+z1+"z = "+s1);
